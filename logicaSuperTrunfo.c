0f1c256c3c964a51c6856e74019a38145f898ad1
Gustavo Sampaio <37808461+gstsdev@users.noreply.github.com>
2025-11-27 01:58:08 +0000

    adiciona cartas pre-cadastradas

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -13,23 +13,23 @@ int main() {
     // Dados da Carta 1
   
     // Uma letra de 'A' a 'H' (representando um dos oito estados). 
-    char carta1Estado;
+    char carta1Estado = 'A';
     
     // A letra do estado seguida de um número de 01 a 04 (ex: A01, B03).
     // Aqui reservamos espaço para 4 caracteres, considerando o caractere
     // nulo (\0) que é adicionando ao final da string.
-    char carta1Codigo[4];
+    char carta1Codigo[4] = "A01";
     
     // O nome da cidade.
-    char carta1Cidade[50];
+    char carta1Cidade[50] = "São Paulo";
     // População: O número de habitantes da cidade.
-    unsigned long int carta1Populacao;
+    unsigned long int carta1Populacao = 12300000;
     // Área (em km²): A área da cidade em quilômetros quadrados.
-    float carta1AreaCidade;
+    float carta1AreaCidade = 1235;
     // PIB: O Produto Interno Bruto da cidade (em bilhões de reais).
-    float carta1PIB;
+    float carta1PIB = 900.66;
     // Número de Pontos Turísticos: A quantidade de pontos turísticos na cidade.
-    int carta1NumeroPontosTuristicos;
+    int carta1NumeroPontosTuristicos = 50;
     // Densidade populacional: a razão entre a população e área da cidade.
     float carta1DensidadePopulacional;
     // PIB per capita: a razão entre a o PIB da cidade e sua população.
@@ -39,23 +39,23 @@ int main() {
     // Dados da Carta 2
     
     // Uma letra de 'A' a 'H' (representando um dos oito estados). 
-    char carta2Estado;
+    char carta2Estado = 'B';
     
     // A letra do estado seguida de um número de 01 a 04 (ex: A01, B03).
     // Aqui reservamos espaço para 4 caracteres, considerando o caractere
     // nulo (\0) que é adicionando ao final da string.
-    char carta2Codigo[4];
+    char carta2Codigo[4] = "B02";
 
     // O nome da cidade.
-    char carta2Cidade[50];
+    char carta2Cidade[50] = "Rio de Janeiro";
     // População: O número de habitantes da cidade.
-    unsigned long int carta2Populacao;
+    unsigned long int carta2Populacao = 6000000;
     // Área (em km²): A área da cidade em quilômetros quadrados.
-    float carta2AreaCidade;
+    float carta2AreaCidade = 1100;
     // PIB: O Produto Interno Bruto da cidade (em bilhões de reais).
-    float carta2PIB;
+    float carta2PIB = 600;
     // Número de Pontos Turísticos: A quantidade de pontos turísticos na cidade.
-    int carta2NumeroPontosTuristicos;
+    int carta2NumeroPontosTuristicos = 30;
     // Densidade populacional: a razão entre a população e área da cidade.
     float carta2DensidadePopulacional;
     // PIB per capita: a razão entre a o PIB da cidade e sua população.
@@ -66,37 +66,37 @@ int main() {
     
     // Insere os dados da carta 1
 
-    printf("==== Carta 1 ====\n");
+    // printf("==== Carta 1 ====\n");
 
-    printf("Informe o Estado da carta (de A a H): ");
-    // Utilizamos um espaço em branco antes do %c para garantir que
-    // as novas linhas (\n) não atrapalhem a leitura do caractere.
-    scanf(" %c", &carta1Estado);
+    // printf("Informe o Estado da carta (de A a H): ");
+    // // Utilizamos um espaço em branco antes do %c para garantir que
+    // // as novas linhas (\n) não atrapalhem a leitura do caractere.
+    // scanf(" %c", &carta1Estado);
     
-    printf("\nInforme o código da carta (letra do Estado + número de 01 a 04, ex: A01, B03): ");
-    scanf("%s", carta1Codigo);
+    // printf("\nInforme o código da carta (letra do Estado + número de 01 a 04, ex: A01, B03): ");
+    // scanf("%s", carta1Codigo);
 
-    printf("\nInforme o nome da cidade: ");
-    // Devemos executar a função `getchar()` para evitar que o `fgets`
-    // leia o caractere \n (nova linha) dos `scanf`s, permitindo
-    // a entrada de texto.
-    getchar();
-    // Utilizamos `fgets()` para permitir nomes de cidades com espaço
-    fgets(carta1Cidade, 50, stdin);
-    // Substitui o caractere \n, incluido por `fgets`, por \0
-    carta1Cidade[strcspn(carta1Cidade, "\n")] = 0;
+    // printf("\nInforme o nome da cidade: ");
+    // // Devemos executar a função `getchar()` para evitar que o `fgets`
+    // // leia o caractere \n (nova linha) dos `scanf`s, permitindo
+    // // a entrada de texto.
+    // getchar();
+    // // Utilizamos `fgets()` para permitir nomes de cidades com espaço
+    // fgets(carta1Cidade, 50, stdin);
+    // // Substitui o caractere \n, incluido por `fgets`, por \0
+    // carta1Cidade[strcspn(carta1Cidade, "\n")] = 0;
 
-    printf("\nInforme o número de habitantes da cidade: ");
-    scanf("%lu", &carta1Populacao);
+    // printf("\nInforme o número de habitantes da cidade: ");
+    // scanf("%lu", &carta1Populacao);
 
-    printf("\nInforme a área da cidade em quilômetros quadrados: ");
-    scanf("%f", &carta1AreaCidade);
+    // printf("\nInforme a área da cidade em quilômetros quadrados: ");
+    // scanf("%f", &carta1AreaCidade);
 
-    printf("\nInforme o valor do PIB da cidade (em bilhões de reais): ");
-    scanf("%f", &carta1PIB);
+    // printf("\nInforme o valor do PIB da cidade (em bilhões de reais): ");
+    // scanf("%f", &carta1PIB);
 
-    printf("\nInforme o número de pontos turísticos da cidade: ");
-    scanf("%d", &carta1NumeroPontosTuristicos);
+    // printf("\nInforme o número de pontos turísticos da cidade: ");
+    // scanf("%d", &carta1NumeroPontosTuristicos);
 
     // Calcula a densidade populacional, utilizando a razão
     // entre o número de habitantes e àrea da cidade.
@@ -109,41 +109,41 @@ int main() {
     // casting do valor da populção para `float`.
     carta1PIBPerCapita = carta1PIB * 1000000000 / (float) carta1Populacao;
 
-    printf("\n=================\n");
+    // printf("\n=================\n");
 
     // Insere os dados da carta 2
 
-    printf("==== Carta 2 ====\n");
+    // printf("==== Carta 2 ====\n");
 
-    printf("Informe o Estado da carta (de A a H): ");
-    // Utilizamos um espaço em branco antes do %c para garantir que
-    // as novas linhas (\n) não atrapalhem a leitura do caractere.
-    scanf(" %c", &carta2Estado);
+    // printf("Informe o Estado da carta (de A a H): ");
+    // // Utilizamos um espaço em branco antes do %c para garantir que
+    // // as novas linhas (\n) não atrapalhem a leitura do caractere.
+    // scanf(" %c", &carta2Estado);
     
-    printf("\nInforme o código da carta (letra do Estado + número de 01 a 04, ex: A01, B03): ");
-    scanf("%s", carta2Codigo);
+    // printf("\nInforme o código da carta (letra do Estado + número de 01 a 04, ex: A01, B03): ");
+    // scanf("%s", carta2Codigo);
 
-    printf("\nInforme o nome da cidade: ");
-    // Devemos executar a função `getchar()` para evitar que o `fgets`
-    // leia o caractere \n (nova linha) dos `scanf`s, permitindo
-    // a entrada de texto.
-    getchar();
-    // Utilizamos `fgets()` para permitir nomes de cidades com espaço
-    fgets(carta2Cidade, 50, stdin);
-    // Substitui o caractere \n, incluido por `fgets`, por \0
-    carta2Cidade[strcspn(carta2Cidade, "\n")] = 0;
+    // printf("\nInforme o nome da cidade: ");
+    // // Devemos executar a função `getchar()` para evitar que o `fgets`
+    // // leia o caractere \n (nova linha) dos `scanf`s, permitindo
+    // // a entrada de texto.
+    // getchar();
+    // // Utilizamos `fgets()` para permitir nomes de cidades com espaço
+    // fgets(carta2Cidade, 50, stdin);
+    // // Substitui o caractere \n, incluido por `fgets`, por \0
+    // carta2Cidade[strcspn(carta2Cidade, "\n")] = 0;
 
-    printf("\nInforme o número de habitantes da cidade: ");
-    scanf("%lu", &carta2Populacao);
+    // printf("\nInforme o número de habitantes da cidade: ");
+    // scanf("%lu", &carta2Populacao);
 
-    printf("\nInforme a área da cidade em quilômetros quadrados: ");
-    scanf("%f", &carta2AreaCidade);
+    // printf("\nInforme a área da cidade em quilômetros quadrados: ");
+    // scanf("%f", &carta2AreaCidade);
 
-    printf("\nInforme o valor do PIB da cidade (em bilhões de reais): ");
-    scanf("%f", &carta2PIB);
+    // printf("\nInforme o valor do PIB da cidade (em bilhões de reais): ");
+    // scanf("%f", &carta2PIB);
 
-    printf("\nInforme o número de pontos turísticos da cidade: ");
-    scanf("%d", &carta2NumeroPontosTuristicos);
+    // printf("\nInforme o número de pontos turísticos da cidade: ");
+    // scanf("%d", &carta2NumeroPontosTuristicos);
 
     // Calcula a densidade populacional, utilizando a razão
     // entre o número de habitantes e àrea da cidade.
@@ -156,7 +156,7 @@ int main() {
     // casting do valor da populção para `float`.
     carta2PIBPerCapita = carta2PIB * 1000000000 / (float) carta2Populacao;
 
-    printf("\n=================\n");
+    // printf("\n=================\n");
 
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
